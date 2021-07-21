869a6c90dfec842ff6cc879719fe55f12a0eb54a
henriquesouzadev <henriquesouzadev@gmail.com>
2021-07-21 14:28:43 -0300

    api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-   baseURL: process.env.API_URL || 'http://localhost:3333/'
+   baseURL: 'https://github.com/henriquesouzadev/podcastr-nextjs/blob/main/db.json' || 'http://localhost:3333/'
 });
